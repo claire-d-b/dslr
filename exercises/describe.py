6cd19263857b5861d12889cd82074aace879d014
claire-d-b <claire.deberny@essca.eu>
2024-12-18 14:11:20 +0100

    Fix code in describe

--- exercises/describe.py
+++ exercises/describe.py
@@ -1,21 +1,23 @@
 from pandas import DataFrame, concat
-from stats import get_mean, get_median, get_standard_deviation, \
-                  get_quartile
-from utils import load
+from stats import get_median, get_standard_deviation, get_quartile
+from utils_figures import load
 
 
 def print_dataframe(df: DataFrame) -> any:
-    """Print statistics per category from a dataframe's values"""
-    # Select the 2nd column (index 1)
-    df_house = df.iloc[:, [1]]
-    print("house", df_house.values)
-    # Select courses
+    """Print statistics per category (house) from a dataframe's values"""
+    # Select houses
+    df_house = df.iloc[:, [0]]
+
+    # Select courses and scores
     df_courses = df.iloc[:, 6:]
-    print("courses", df_courses)
 
     df = concat([df_house, df_courses], axis=1)
-    # Group by house and sum up values for each course accordingly
-    df = df.groupby('Hogwarts House').sum()
+    # Group by house
+    df = df.groupby('Hogwarts House')
+    # How many students per house
+    houses_count = df.size()
+    # sum up courses scores per house
+    df = df.sum()
 
     indexes = list(df.index)
 
@@ -28,7 +30,7 @@ def print_dataframe(df: DataFrame) -> any:
         # Get scores for all 12 courses in a specific house
         row_values = [value for value in df.iloc[i]]
 
-        values[i].append(get_mean(row_values))
+        values[i].append(houses_count.iloc[i])
         # print("variance:", get_variance(row_values))
         values[i].append(get_standard_deviation(row_values))
         values[i].append(min(row_values))
