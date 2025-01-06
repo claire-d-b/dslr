6ed7745cc7a6e9ec610066df809db7015fb3e725
claire-d-b <claire.deberny@essca.eu>
2025-01-06 19:10:12 +0100

    normalization of data - ex01

--- exercises/histogram.py
+++ exercises/histogram.py
@@ -1,7 +1,7 @@
 # scores / standard deviation / range
 from pandas import DataFrame, concat
 from matplotlib.pyplot import savefig, tight_layout, subplots, \
-                              xlabel, ylabel, title, legend
+                              xlabel, ylabel, title, legend, bar
 from numpy import arange
 from matplotlib.patches import Rectangle
 from utils_figures import load, normalize_column
@@ -14,30 +14,31 @@ def get_bars(df: DataFrame) -> any:
     # Select the 2nd column (index 1)
     df_house = df.iloc[:, [0]]
     # Select columns starting from 7th (index 6) onward
-    df_courses = df.iloc[:, 6:]
+    df_courses = df.iloc[:, 5:]
+
     min_value = df_courses.min()
     max_value = df_courses.max()
     df_courses = normalize_column(df_courses, min_value, max_value)
     df = concat([df_house, df_courses], axis=1)
+    print("DF=", df)
+    # table = []
 
-    table = []
+    # for i in range(df.shape[0]):
+    #     table.insert(i, [])
 
-    for i in range(df.shape[0]):
-        table.insert(i, [])
+    #     for j in range(df.shape[1]):
 
-        for j in range(df.shape[1]):
+    #         if j == 0:
+    #             table[i].insert(j, df.iloc[[i], [j]].values[0][0])
+    #         else:
+    #             table[i].insert(j, float(df.iloc[[i], [j]].values[0][0]))
 
-            if j == 0:
-                table[i].insert(j, df.iloc[[i], [j]].values[0][0])
-            else:
-                table[i].insert(j, float(df.iloc[[i], [j]].values[0][0]))
+    # table = sorted(table)
+    # ntable = DataFrame(table)
 
-    table = sorted(table)
-    ntable = DataFrame(table)
-
-    # Group by 'Hogwarts House' and sum up all columns so we get a table with
-    # one row per house
-    grouped = ntable.groupby(0)[ntable.columns[1:]].sum().reset_index()
+    # # Group by 'Hogwarts House' and sum up all columns so we get a table with
+    # # one row per house
+    # grouped = ntable.groupby(0)[ntable.columns[1:]].sum().reset_index()
 
     fig, ax = subplots(figsize=(8, 6))
     x = arange(len(houses))
@@ -47,21 +48,50 @@ def get_bars(df: DataFrame) -> any:
 
     handles = [Rectangle((0, 0), 1, 1, color=color) for color in colors]
 
-    for i, house in enumerate(houses):
-        values = [float(x) for x in grouped.iloc[i][0:].values[1:]]
+    counts = load("describe.csv")
 
-        x = [int(x) for x in grouped.iloc[i].index[1:]]
+    print("counts", counts.iloc[[0], :])
+    # for i, unit in enumerate(counts.iloc[[0], :].values):
+    #     print("unit", unit)
+    table = []
+    ndf = df.groupby('Hogwarts House').sum()
+    for i, (unit, house) in enumerate(zip(counts.iloc[[0], :].values, ndf.values)):
+        table.insert(i, [])
+        for j, course in enumerate(house):
+            print("unit", unit)
+            print("houze", course)
+            print("j", j)
+            course /= unit
+            table[i].insert(j, course)
 
-        for j, metric in enumerate(values):
+    table = DataFrame(table)
+    # table.columns = ndf.columns
+    print("table", table)
+
+    # scores_per_house_per_course = table
+    # values = ndf.iloc[:, 0:].values
+    # print("valz", values)
+    # print("cvalz", df_courses.values)
+    print("result", len(table.columns))
+    # result = table
+    for i, col in enumerate(table.columns):
+        print('i:', i)
+        
+        course = table[col][0]
+        print("course", course)
+
+        for j, metric in enumerate(course):
+            print("metric", metric)
+            print("j:", j)
             # X-axis positions for each group of bars
-            ax.bar(x[j] + size[i] * bar_width, metric, width=bar_width,
-                   label=house, color=colors[i])
+            bar(i + size[j] * bar_width, metric, width=bar_width,
+                   label=houses[j], color=colors[j])
 
     tight_layout()
     legend(handles=handles, labels=houses)
     # ylim(-1000, 1000)
-    xlabel("Course n°")
-    ylabel("Scores")
+    # xlabel("Course n°")
+    # ylabel("Scores")
     title("Histogram of Data")
     savefig("histogram")
 
