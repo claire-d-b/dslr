058be403fd1a42cde6e2c29a6c097a9b9053a7cc
claire-d-b <claire.deberny@essca.eu>
2025-01-22 17:36:08 +0100

    wip cost function

--- cost.py
+++ cost.py
@@ -10,10 +10,14 @@ from matplotlib.pyplot import savefig, show, \
                               title, figure
 
 
+HOUSE = 'Ravenclaw'
+HOUSE_INDEX = 2
+
 def get_cost() -> any:
     houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
     ndf = load("dataset_train.csv")
+    ndf = ndf.fillna(0)
 
     lhs = ndf['Hogwarts House']
     rhs = ndf.iloc[:, 5:]
@@ -38,8 +42,11 @@ def get_cost() -> any:
     theta_1 = w
 
     # Choose here 0, 1, 2 or 3 depending on the house you want to analyze below
-    real_houses = [1 if houses.index(h) == 0 else 0 for h in lhs]
-
+    real_houses = [1 if houses.index(h) == HOUSE_INDEX else 0 for h in lhs]
+    # print("real houses", real_houses)
+    # Choose here 'Gryffindor', 'Hufflepuff", "Ravenclaw", "Slytherin"
+    # depending on the house you want to analyze below
+    name = HOUSE
     # meshgrid is a function from the numpy library used to create coordinate grids,
     # typically for 3D plotting or evaluating functions over a 2D domain
     # numpy.meshgrid(*xi, indexing='xy')
@@ -62,7 +69,8 @@ def get_cost() -> any:
             # Mean along axis 0 (columns): this calculates the mean for each column (across the rows).
             # The result will be a 1D array with the shape (13,):
             # print("shape theta_1:", mean(theta_1, axis=0).shape)
-            t_1 = mean(theta_1, axis=0)[j]
+            # t_1 = mean(theta_1, axis=0)[j]
+            t_1 = theta_1[HOUSE_INDEX][j]
             z = A[i, j] * t_1 + theta_0
             y_hat[i, j] = 1 / (1 + e ** -z)
             loss[i, j] = -B[i, j] * log(y_hat[i, j]) - (1 - B[i, j]) * log(1 - y_hat[i, j])
@@ -72,7 +80,7 @@ def get_cost() -> any:
     surface = ax.plot_surface(A, B, loss, cmap='cool',
                               edgecolor='none')
     fig.colorbar(surface, ax=ax, shrink=0.5, aspect=5)
-    title("Cost function in logistic regression")
+    title(f"Cost function: {name}")
     ax.set_xlabel("scores")
     ax.set_ylabel("house")
     ax.set_zlabel("cost")
