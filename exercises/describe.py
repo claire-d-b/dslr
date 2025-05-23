dad09f363b6d7ba32a6ede0ce257bf257dcf846f
claire-d-b <claire.deberny@essca.eu>
2025-05-23 18:19:37 +0200

    remove math functions from modules

--- exercises/describe.py
+++ exercises/describe.py
@@ -1,8 +1,8 @@
 from pandas import DataFrame, concat
 from stats import get_median, get_standard_deviation, get_quartile, get_min, get_max, get_mean, len, get_mean
 from utils_figures import load
-from math import isnan
-from numpy import percentile, std
+# from math import isnan
+# from numpy import percentile, std
 
 
 def print_dataframe(df: DataFrame) -> any:
@@ -24,12 +24,12 @@ def print_dataframe(df: DataFrame) -> any:
     df.dropna()
 
     values = []
-    nvalues = []
+    # nvalues = []
 
     data = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
     ndf = DataFrame(data)
 
-    nndf = DataFrame(data)
+    # nndf = DataFrame(data)
 
     for i in range(df.shape[1]):
         row_values = df.iloc[:, i].dropna().values
@@ -39,52 +39,52 @@ def print_dataframe(df: DataFrame) -> any:
 
         # print("i:", i)
         values.insert(i, [])
-        nvalues.insert(i, [])
+        # nvalues.insert(i, [])
         # print("row values shape", DataFrame(row_values).shape)
         values[i].append(len(nrow_values))
-        nvalues[i].append(len(nrow_values))
+        # nvalues[i].append(len(nrow_values))
 
         # print("courses_count i", stud_per_course_count)
         values[i].append(get_mean(nrow_values))
-        nvalues[i].append(nrow_values.mean())
+        # nvalues[i].append(nrow_values.mean())
 
         values[i].append(get_standard_deviation(nrow_values))
-        nvalues[i].append(std(nrow_values))
+        # nvalues[i].append(std(nrow_values))
 
         values[i].append(get_min(nrow_values))
-        nvalues[i].append(nrow_values.min())
+        # nvalues[i].append(nrow_values.min())
 
         values[i].append(get_quartile(nrow_values)[0])
-        nvalues[i].append(percentile(nrow_values, 25))
+        # nvalues[i].append(percentile(nrow_values, 25))
 
         values[i].append(get_median(nrow_values))
-        nvalues[i].append(percentile(nrow_values, 50))
+        # nvalues[i].append(percentile(nrow_values, 50))
 
         values[i].append(get_quartile(nrow_values)[1])
-        nvalues[i].append(percentile(nrow_values, 75))
+        # nvalues[i].append(percentile(nrow_values, 75))
 
         values[i].append(get_max(nrow_values))
-        nvalues[i].append(nrow_values.max())
+        # nvalues[i].append(nrow_values.max())
 
         # Remove NaN values before calculating
         row_values = [x for x in nrow_values if not isnan(x)]
         ndf = concat([ndf, DataFrame(values[i])], axis=1)
-        nndf = concat([nndf, DataFrame(nvalues[i])], axis=1)
+        # nndf = concat([nndf, DataFrame(nvalues[i])], axis=1)
 
     # Get the current columns
     columns = ndf.columns.tolist()
-    nncolumns = nndf.columns.tolist()
+    # nncolumns = nndf.columns.tolist()
     # print("ncolumns")
     # # Rename columns to new names
     columns[1:14] = ncolumns
-    nncolumns[1:14] = ncolumns
+    # nncolumns[1:14] = ncolumns
     # # Assign the new column names back to the DataFrame
     ndf.columns = columns
-    nndf.columns = nncolumns
+    # nndf.columns = nncolumns
 
     # Write the entire DataFrame to a CSV file
     ndf.to_csv("describe.csv", index=False)
-    nndf.to_csv("describe_truth.csv", index=False)
+    # nndf.to_csv("describe_truth.csv", index=False)
 
     # A downward-sloping diagonal (from top-left to bottom-right) indicates
     # a negative correlation, where as one variable increases, the other
